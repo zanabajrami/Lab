17621f03d37e1ef735cad515376e01dd4e64180c
zanabajrami <149783174+zanabajrami@users.noreply.github.com>
2025-12-26 23:50:07 +0100

    Update User model for last_login_at

--- booking/backend/app/Models/User.php
+++ booking/backend/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
@@ -12,13 +11,25 @@ class User extends Authenticatable implements JWTSubject
     use Notifiable;
 
     protected $fillable = [
-        'first_name', 'last_name', 'email', 'password','role',
+        'first_name', 'last_name', 'email', 'password', 'role',
     ];
 
     protected $hidden = [
         'password',
     ];
 
+    public $timestamps = true;
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'last_login_at' => 'datetime',
+    ];
+
+    // JWT functions
     public function getJWTIdentifier()
     {
         return $this->getKey();
